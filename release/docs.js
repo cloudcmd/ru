04a29791c08bb80340c11f3807b45e097637b106
coderaiser <coderaiser@cloudcmd.io>
2018-08-13 14:31:57 +0300

    chore(docs) es2015-ify

--- release/docs.js
+++ release/docs.js
@@ -1,56 +1,49 @@
-(function() {
-    'use strict';
+'use strict';
+
+const DIR = '../';
+const cl = require('./cl');
+const place = require('place');
+const rendy = require('rendy');
+const shortdate = require('shortdate');
+const files = require('files-io');
+const {version} = require(DIR + 'package');
+
+module.exports = (callback) => {
+    const name = 'Історія версій';
+    const history = name + '\n---------------\n';
+    const link = '//github.com/coderaiser/cloudcmd/releases/tag/';
+    const template = '- *{{ date }}*, '     +
+                  '**[v{{ version }}]'      +
+                  '(' + link + 'v{{ version }})**\n';
     
-    var DIR         = '../',
+    cl((error, versionNew) => {
+        if (error)
+            return callback(error);
         
-        cl          = require('./cl'),
-        place       = require('place'),
-        rendy       = require('rendy'),
-        shortdate   = require('shortdate'),
-        files       = require('files-io'),
-        Info        = require(DIR + 'package');
-        
-    module.exports = function(callback) {
-        var name        = 'История версий',
-            history     = name + '\n---------------\n',
-            link        = '//github.com/coderaiser/cloudcmd/releases/tag/',
-            template    = '- *{{ date }}*, '    +
-                          '**[v{{ version }}]'   +
-                          '(' + link + 'v{{ version }})**\n',
-            version     = Info.version;
-        
-        cl(function(error, versionNew) {
-            if (error) {
-                callback(error);
-            } else {
-                replaceVersion('index.md', version, versionNew, function() {
-                    var historyNew = history + rendy(template, {
-                        date        : shortdate(),
-                        version     : versionNew
-                    });
-                    
-                    replaceVersion('index.md', history, historyNew, function() {
-                        var to = rendy('v{{ version }}.md', {
-                            version: versionNew
-                        });
-                        
-                        files.pipe('index.md', to, function(error) {
-                            callback(error, 'done: ok');
-                        });
-                    });
+        replaceVersion('index.md', version, versionNew, () => {
+            const historyNew = history + rendy(template, {
+                date        : shortdate(),
+                version     : versionNew
+            });
+            
+            replaceVersion('index.md', history, historyNew, () => {
+                const to = rendy('v{{ version }}.md', {
+                    version: versionNew
                 });
-            }
+                
+                files.pipe('index.md', to, (error) => {
+                    callback(error, 'done: ok');
+                });
+            });
         });
-    };
-    
-    function replaceVersion(name, version, versionNew, callback) {
-        place(name, version, versionNew, function(error) {
-            var msg;
-            
-            if (!error)
-                msg = 'done: ' + name;
-            
-            callback(error, msg);
-        });
-    }
-})();
+    });
+};
+
+function replaceVersion(name, version, versionNew, callback) {
+    place(name, version, versionNew, (error) => {
+        if (error)
+            return callback(error);
+        
+        callback(null, 'done: ' + name);
+    });
+}
